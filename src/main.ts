6a23994958511bac6077680de6a7d2f51396f02c
miguel javela <mjavela@compuamerica.com.ve>
2022-05-19 23:06:24 -0500

    clase 9 - numbers, boolean y string

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,36 @@
 console.log('hola main jjjj');
+
+//number
+//explicito
+let phone: number;
+phone = 1; 
+//inferido
+let phoneNumber = 3434343;
+//hexadecimal
+let hex: number = 0xf00d;
+//binario
+let binary:number = 0b1010;
+//octarl
+let octal: number = 0o744;
+
+//boolean
+let isPro: boolean;
+isPro = true;
+//isPro = 1 //error
+//inferido
+let isUserPro = false;
+
+//String
+let username: string = "peña";
+
+//template string
+//uso de back-tick `
+let userInfo: string;
+userInfo = `
+    user info:
+    username: ${username}
+    firstName: ${username + 'aviles'}
+    phone: ${phone}
+    isPro: ${isPro}
+`;
+console.log('userInfo',userInfo);
