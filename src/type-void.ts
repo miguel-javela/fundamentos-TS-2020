7bd48a3c784a4bb9bc3b9ace8a2fd4931418263d
miguel javela <mjavela@compuamerica.com.ve>
2022-05-20 19:59:11 -0500

    clase - 11 void y never

---
+++ src/type-void.ts
@@ -0,0 +1,46 @@
+//void
+//tipo explicito
+function showInfo (user:any):any{
+    console.log('user info', user.id, user.username, user.firstname);
+    //return 'hola';
+}
+
+showInfo({id:1, username:'migueljavela', firstname: 'miguel'});
+
+//tipo inferido
+function showFormattedInfo(user:any){
+    console.log('user info',`
+        id: ${user.id}
+        username:${user.username}
+        firstname: ${user.firstname}
+    `)
+}
+showFormattedInfo({id:1, username:'migueljavela', firstname: 'miguel'});
+
+//tipo void, como tipo de dato de una variable
+let  unusuable : void;
+unusuable = null;
+unusuable = undefined
+
+//tipo: never
+
+function handleError (code: number, message: string):never{
+    //process your code
+    //generamos un mensaje
+    throw new Error(`${message}. code: ${code}`);
+}
+
+try{
+    handleError(404,"not found");
+} catch(error) {
+
+}
+
+//
+function sumNumbers(lim:number): never{
+    let sum = 0;
+    while(true){
+        sum++;
+    }
+}
+sumNumbers(10);
